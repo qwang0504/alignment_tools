843478e8fb49cc4a1efb224af6c12dacc4cfbe1f
ElTinmar <martin.privat@hotmail.fr>
2024-02-28 17:02:47 +0100

    update

--- alignment_tools/test.py
+++ alignment_tools/test.py
@@ -3,10 +3,10 @@ from PyQt5.QtWidgets import QApplication
 import sys
 import cv2
 
-FIXED = 'toy_data/image_00.jpg'
-MOVING = 'toy_data/image_02.jpg'
+FIXED = cv2.imread('toy_data/image_00.jpg')[:,:,0]
+MOVING = cv2.imread('toy_data/image_02.jpg')[:,:,0]
 
 app = QApplication(sys.argv)
-window = AlignAffine2D(cv2.imread(FIXED)[:,:,0], cv2.imread(MOVING)[:,:,0])
+window = AlignAffine2D(FIXED, MOVING)
 window.show()
 app.exec()
